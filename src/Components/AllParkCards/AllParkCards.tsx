b99021c62177f29eb8d8294ef9440d00ce6c4f73
Ethan Groene <ethangroene@gmail.com>
2023-12-16 00:22:09 -0500

    Display info for 18 parks at a time
    Also, make button that increments the amount of displayed parks by 18 until all are displayed, upon which the button disappears.

--- src/Components/AllParkCards/AllParkCards.tsx
+++ src/Components/AllParkCards/AllParkCards.tsx
@@ -4,12 +4,29 @@ import { ParkCard } from "../ParkCard/ParkCard";
 
 // map inside this comp to create ParkCard for every park currently in allNationalParks
 export const AllParkCards = () => {
-  const { allNationalParks } = useMainContentContext();
+  const { allNationalParks, limit, setLimit } = useMainContentContext();
+  const displayedParks = allNationalParks.filter(
+    (park) => allNationalParks.indexOf(park) < limit
+  );
+
   return (
     <>
-      {allNationalParks.map((park: TPark) => (
-        <ParkCard park={park} />
-      ))}
+      <div className="card-container">
+        {displayedParks.map((park: TPark) => (
+          <ParkCard park={park} />
+        ))}
+      </div>
+      {displayedParks.length !== allNationalParks.length && (
+        <button
+          onClick={() => {
+            limit < allNationalParks.length
+              ? setLimit(limit + 18)
+              : setLimit(allNationalParks.length);
+          }}
+        >
+          Increase limit
+        </button>
+      )}
     </>
   );
 };
