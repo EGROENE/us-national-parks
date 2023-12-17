36c35e29c544057786818f91aad4bf2f7c6de900
Ethan Groene <ethangroene@gmail.com>
2023-12-17 14:27:22 -0500

    Add img, title, desc for every park
    Img includes ability to rotate b/t images that park has. This is done by clicking arrow icons.

--- src/Components/ParkCard/ParkCard.tsx
+++ src/Components/ParkCard/ParkCard.tsx
@@ -1,13 +1,50 @@
+import { useState } from "react";
 import { TPark } from "../../types";
 
 type TParkCardProps = {
   park: TPark;
 };
 
+type TDirection = "next" | "prev";
+
 export const ParkCard = ({ park }: TParkCardProps) => {
+  const [imgIndex, setImgIndex] = useState<number>(
+    Math.floor(Math.random() * park.images.length)
+  );
+
+  const changeThumbnail = (
+    direction: TDirection,
+    imgIndex: number,
+    imgArray: {
+      credit: string;
+      title: string;
+      altText: string;
+      caption: string;
+      url: string;
+    }[]
+  ): void => {
+    if (direction === "next") {
+      imgIndex === imgArray.length - 1 ? setImgIndex(0) : setImgIndex(imgIndex + 1);
+    } else {
+      imgIndex === 0 ? setImgIndex(imgArray.length - 1) : setImgIndex(imgIndex - 1);
+    }
+  };
+
   return (
-    <>
-      <h1>{park.fullName}</h1>
-    </>
+    <div className="park-card">
+      <div className="park-thumbnail-container">
+        <i
+          onClick={() => changeThumbnail("prev", imgIndex, park.images)}
+          className="fas fa-angle-right"
+        ></i>
+        <img src={park.images[`${imgIndex}`].url} alt="" />
+        <i
+          onClick={() => changeThumbnail("next", imgIndex, park.images)}
+          className="fas fa-angle-right"
+        ></i>
+      </div>
+      <header>{park.fullName}</header>
+      <p>{park.description}</p>
+    </div>
   );
 };
