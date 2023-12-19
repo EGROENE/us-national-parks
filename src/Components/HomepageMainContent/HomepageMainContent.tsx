c12c4c3a49fc74d2ba74cad64e86c82c5f1a2cb3
Ethan Groene <ethangroene@gmail.com>
2023-12-19 15:57:27 -0500

    Add, style scroll-to-top' btn
    Appears when user scrolls over 150px from top on homepage, then disappears if <= 150

--- src/Components/HomepageMainContent/HomepageMainContent.tsx
+++ src/Components/HomepageMainContent/HomepageMainContent.tsx
@@ -1,6 +1,28 @@
 // AllParkCards (child ParkCard) & SearchTools should be this comp's children
+import { useEffect, useState } from "react";
 import { AllParkCards } from "../AllParkCards/AllParkCards";
+import { ScrollToTopBtn } from "../ScrollToTopBtn/ScrolltoTopBtn";
 
 export const HomepageMainContent = () => {
-  return <AllParkCards />;
+  const [distanceScrolledFromTop, setDistanceScrolledFromTop] = useState<number>(0);
+
+  // Remember, any time useEffect adds an EL, it should be removed in its 'return' statement
+  useEffect(() => {
+    const handleScroll = () => {
+      setDistanceScrolledFromTop(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <AllParkCards />
+      <ScrollToTopBtn distanceScrolledFromTop={distanceScrolledFromTop} />
+    </>
+  );
 };
---
+++ src/Components/ScrollToTopBtn/ScrollToTopBtn.tsx
@@ -0,0 +1,18 @@
+export const ScrollToTopBtn = ({
+  distanceScrolledFromTop,
+}: {
+  distanceScrolledFromTop: number;
+}) => {
+  return (
+    <i
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      className={
+        distanceScrolledFromTop > 150
+          ? "fas fa-angle-right animate__animated animate__fadeInUp"
+          : "fas fa-angle-right animate__animated animate__fadeOutDown"
+      }
+      title="Back to Top"
+      id="scroll-to-top-btn"
+    ></i>
+  );
+};
