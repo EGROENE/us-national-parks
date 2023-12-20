d0c14fcd1a44987cc92d8ac12045b6dc840d119a
Ethan Groene <ethangroene@gmail.com>
2023-12-19 21:34:14 -0500

    Various styling
    Decrease size of scroll-to-top btn, increase top margin of init loading/error messages.

--- src/Components/FailInitFetchMessage/FailInitFetchMessage.tsx
+++ src/Components/FailInitFetchMessage/FailInitFetchMessage.tsx
@@ -1,7 +1,9 @@
 export const FailInitFetchMessage = () => {
   return (
     <>
-      <p>Sorry, there was an error in fetching the data. Please reload the page.</p>
+      <p className="init-loading-or-error-message">
+        Sorry, there was an error in fetching the data. Please reload the page.
+      </p>
       <button onClick={() => window.location.reload()}>Reload Page</button>
     </>
   );
--- src/Components/LoadingMessage/LoadingMessage.tsx
+++ src/Components/LoadingMessage/LoadingMessage.tsx
@@ -1,4 +1,8 @@
 // Should return message & loading animation
 export const LoadingMessage = () => {
-  return <p>Wait a sec while we retrieve the data...</p>;
+  return (
+    <p className="init-loading-or-error-message">
+      Wait a sec while we retrieve the data...
+    </p>
+  );
 };
