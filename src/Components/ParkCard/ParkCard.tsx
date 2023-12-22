3c0d3f575dcfc014c039119db0fd860c3c7c983d
Ethan Groene <ethangroene@gmail.com>
2023-12-22 17:17:19 -0500

    Ensure state/terr labels render appropriately

--- src/Components/ParkCard/ParkCard.tsx
+++ src/Components/ParkCard/ParkCard.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { TPark } from "../../types";
-import { stateFilterOptions } from "../../constants";
+import { stateFilterOptions, territoryFilterOptions } from "../../constants";
 
 type TDirection = "next" | "prev";
 
@@ -27,13 +27,45 @@ export const ParkCard = ({ park }: { park: TPark }) => {
     }
   };
 
-  const parkStates = (): string => {
-    if (park.states.length > 3) {
-      const stateIndices = park.states.replace(/,/g, " ").split(" ");
-      return stateIndices.map((index) => stateFilterOptions[index]).join(", ");
-    }
-    return stateFilterOptions[`${park.states}`];
+  // maybe return arrays from these, .join(", ") in JSX
+  const parkStates = (): string[] => {
+    const stateIndices = park.states
+      .replace(/,/g, " ")
+      .split(" ")
+      .filter((index) => Object.keys(stateFilterOptions).includes(index));
+    return stateIndices.map((index) => stateFilterOptions[index]);
   };
+
+  const parkTerritories = (): string[] => {
+    const territoryIndices = park.states
+      .replace(/,/g, " ")
+      .split(" ")
+      .filter((index) => Object.keys(territoryFilterOptions).includes(index));
+    return territoryIndices.map((index) => territoryFilterOptions[index]);
+  };
+
+  // All possible combinations of where a park could be (in reality, only a couple of these will likely actually be used)
+  // Ensures wording of state/territory labels are grammatically correct & appear only if park is in a state/territory
+  const isInOneTerritoryAndOneState =
+    parkStates().length === 1 && parkTerritories().length === 1;
+
+  const isInSeveralTerritoriesAndOneState =
+    parkTerritories().length > 1 && parkStates().length === 1;
+
+  const isInSeveralStatesAndOneTerritory =
+    parkStates().length > 1 && parkTerritories().length === 1;
+
+  const isInOneTerritoryAndNoStates =
+    parkTerritories().length === 1 && !parkStates().length;
+
+  const isInSeveralTerritoriesAndNoStates =
+    parkTerritories().length > 1 && !parkStates().length;
+
+  const isInOneStateAndNoTerritories =
+    parkStates().length === 1 && !parkTerritories().length;
+
+  const isInSeveralStatesAndNoTerritories =
+    parkStates().length > 1 && !parkTerritories().length;
 
   return (
     <div className="park-card">
@@ -58,10 +90,23 @@ export const ParkCard = ({ park }: { park: TPark }) => {
         )}
       </div>
       <header>{park.fullName}</header>
-      <p className="state-list">
-        <span>{park.states.length > 3 ? "States: " : "State: "} </span>
-        {parkStates()}
-      </p>
+      <div>
+        {isInOneTerritoryAndOneState && <p>State: {parkStates().join(", ")}</p> && (
+          <p>Territory: {parkTerritories().join(", ")}</p>
+        )}
+        {isInSeveralTerritoriesAndOneState && <p>State: {parkStates().join(", ")}</p> && (
+          <p>Territories: {parkTerritories().join(", ")}</p>
+        )}
+        {isInSeveralStatesAndOneTerritory && <p>States: {parkStates().join(", ")}</p> && (
+          <p>Territory: {parkTerritories().join(", ")}</p>
+        )}
+        {isInOneTerritoryAndNoStates && <p>Territory: {parkTerritories().join(", ")}</p>}
+        {isInSeveralTerritoriesAndNoStates && (
+          <p>Territories: {parkTerritories().join(", ")}</p>
+        )}
+        {isInOneStateAndNoTerritories && <p>State: {parkStates().join(", ")}</p>}
+        {isInSeveralStatesAndNoTerritories && <p>States: {parkStates().join(", ")}</p>}
+      </div>
       <p>{park.description}</p>
     </div>
   );
