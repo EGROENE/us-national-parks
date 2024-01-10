48aae5dc4a76467a1a8ccdecef3ef2f2e8f8ed1f
Ethan Groene <ethangroene@gmail.com>
2024-01-09 23:34:40 -0600

    Add method to specifically sort park activities

--- src/methods.ts
+++ src/methods.ts
@@ -1,4 +1,25 @@
 import { TPark } from "./types";
+type TParkActivities = [
+  {
+    id: string;
+    name: string;
+  }
+];
+
+/* export const getObjectArraySortedAlphabeticallyByProperty = (
+  array: TParkActivities | TPark[],
+  property
+) => {
+  return array.sort(function (a, b) {
+    if (a[property] < b[property]) {
+      return -1;
+    }
+    if (a[property] > b[property]) {
+      return 1;
+    }
+    return 0;
+  });
+}; */
 
 export const getParksSortedAlphabeticallyByFullName = (array: Array<TPark>) => {
   return array.sort(function (a, b) {
@@ -6,6 +27,18 @@ export const getParksSortedAlphabeticallyByFullName = (array: Array<TPark>) => {
       return -1;
     }
     if (a.fullName > b.fullName) {
+      return 1;
+    }
+    return 0;
+  });
+};
+
+export const getParkActivitiesSortedAlphabeticallyByName = (array: TParkActivities) => {
+  return array.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
       return 1;
     }
     return 0;
