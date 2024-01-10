2dcdb0eea36de8a9951fc0e72fad634cc26f0b75
Ethan Groene <ethangroene@gmail.com>
2024-01-10 12:12:15 -0600

    If filter is territory,get name from rel object
    If territory, then get from territoryFilterOptions; if state, get from stateFilterOptions.

--- src/Components/NoFilterResultsMessage/NoFilterResultsMessage.tsx
+++ src/Components/NoFilterResultsMessage/NoFilterResultsMessage.tsx
@@ -1,7 +1,15 @@
 import { useMainContentContext } from "../../Hooks/useMainContentContext";
-import { stateFilterOptions } from "../../constants";
+import { stateFilterOptions, territoryFilterOptions } from "../../constants";
 
 export const NoFilterResultsMessage = () => {
   const { stateFilter } = useMainContentContext();
-  return <p>{`No national parks exist in ${stateFilterOptions[`${stateFilter}`]}`}</p>;
+  return Object.keys(territoryFilterOptions).indexOf(stateFilter) !== -1 ? (
+    <p>{`No national parks exist in ${
+      territoryFilterOptions[`${stateFilter as keyof typeof territoryFilterOptions}`]
+    }`}</p>
+  ) : (
+    <p>{`No national parks exist in ${
+      stateFilterOptions[`${stateFilter as keyof typeof stateFilterOptions}`]
+    }`}</p>
+  );
 };
