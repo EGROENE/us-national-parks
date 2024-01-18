1053f7fe9b30d7e9aa02a69354fc1520247a8a52
Ethan Groene <ethangroene@gmail.com>
2024-01-17 18:06:23 -0600

    Display diff msg if error code 429

--- src/Components/FailInitFetchMessage/FailInitFetchMessage.tsx
+++ src/Components/FailInitFetchMessage/FailInitFetchMessage.tsx
@@ -1,10 +1,12 @@
-export const FailInitFetchMessage = () => {
+export const FailInitFetchMessage = ({ errorCode }: { errorCode?: string }) => {
   return (
-    <>
+    <div className="fail-fetch-message-container">
       <p className="init-loading-or-error-message">
-        Sorry, there was an error in fetching the data. Please reload the page.
+        {errorCode === "429"
+          ? "Too many requests have been made to the server. Please try again later."
+          : "Sorry, there was an error in fetching the data. Please reload the page."}
       </p>
       <button onClick={() => window.location.reload()}>Reload Page</button>
-    </>
+    </div>
   );
 };
