26c8217456939f3c2cb97d90f51c6ae4e63632aa
Ethan Groene <ethangroene@gmail.com>
2024-01-24 11:25:56 -0600

    Make more reusable by accepting style props

--- src/Components/FailInitFetchMessage/FailInitFetchMessage.tsx
+++ src/Components/FailInitFetchMessage/FailInitFetchMessage.tsx
@@ -1,12 +1,26 @@
-export const FailInitFetchMessage = ({ errorCode }: { errorCode?: string }) => {
+export const FailInitFetchMessage = ({
+  errorCode,
+  margin,
+  buttonColor,
+}: {
+  errorCode?: string;
+  margin?: string;
+  buttonColor?: string;
+}) => {
   return (
     <div className="fail-fetch-message-container">
-      <p className="init-loading-or-error-message">
+      <p style={{ margin: margin }} className="init-loading-or-error-message">
         {errorCode === "429"
           ? "Too many requests have been made to the server. Please try again later."
           : "Sorry, there was an error in fetching the data. Please reload the page."}
       </p>
-      <button onClick={() => window.location.reload()}>Reload Page</button>
+      <button
+        className="reload-btn"
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => window.location.reload()}
+      >
+        Reload Page
+      </button>
     </div>
   );
 };
