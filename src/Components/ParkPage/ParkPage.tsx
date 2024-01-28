4b1306dc7f5651e066e1fead1d26ad3c2505f9f8
Ethan Groene <ethangroene@gmail.com>
2024-01-27 23:06:24 -0600

    Set park to derived state,del conditions
    In order to reduce requests made to API, park data corresponding to the park card the user clicks on, is passed to this component using derived state value from allNationalParks, which is set on init app load. Unneeded conditions (if !park) are deleted, as this will now always be truthy.

--- src/Components/ParkPage/ParkPage.tsx
+++ src/Components/ParkPage/ParkPage.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-// API methods(s):
-import { getParkByCode, getParkCurrentWeather } from "../../api";
+// API method(s):
+import { getParkCurrentWeather } from "../../api";
 
 // Type(s):
-import { TPark, TParkAlert, TCurrentWeather } from "../../types";
+import { TParkAlert, TCurrentWeather } from "../../types";
 
 // Constants:
 import { stateFilterOptions, territoryFilterOptions } from "../../constants";
@@ -19,23 +19,22 @@ import { ParkEntranceFees } from "../ParkPageItems/ParkEntranceFees/ParkEntrance
 import { ParkEntrancePasses } from "../ParkPageItems/ParkEntrancePasses/ParkEntrancePasses";
 import { ParkContacts } from "../ParkPageItems/ParkContacts/ParkContacts";
 import { ParkAlerts } from "../ParkPageItems/ParkAlerts/ParkAlerts";
-import { LoadingMessage } from "../LoadingMessage/LoadingMessage";
-import { FailFetchMessage } from "../FailFetchMessage/FailFetchMessage";
-
-// Hook(s):
-import { useMainContentContext } from "../../Hooks/useMainContentContext";
 import { ParkCurrentWeather } from "../ParkPageItems/ParkCurrentWeather/ParkCurrentWeather";
 import { UniversalFooter } from "../UniversalFooter/UniversalFooter";
 
+// Hook(s):
+import { useMainContentContext } from "../../Hooks/useMainContentContext";
+
 export const ParkPage = () => {
-  const [errorCode, setErrorCode] = useState<string>("");
   const [wasErrorFetchingWeather, setWasErrorFetchingWeather] = useState<boolean>(false);
 
   // Values relating to park info (alerts, current weather, etc.):
-  const { allNPAlerts } = useMainContentContext();
+  const { allNPAlerts, allNationalParks } = useMainContentContext();
+  const parkAlerts: TParkAlert[] = allNPAlerts.filter(
+    (alert) => alert.parkCode === parkCode
+  );
+  const areAlerts: boolean = parkAlerts.length > 0;
   const { parkCode } = useParams();
-  const [park, setPark] = useState<TPark | undefined>();
-  const [parkIsLoading, setParkIsLoading] = useState<boolean>(true);
   const [parkWeather, setParkWeather] = useState<TCurrentWeather | undefined>();
 
   // State values dictating if certain park info should be shown (changed by user & hidden by default):
@@ -46,45 +45,31 @@ export const ParkPage = () => {
   const [showAlerts, setShowAlerts] = useState<boolean>(false);
   const [showCurrentWeather, setShowCurrentWeather] = useState<boolean>(false);
 
-  document.title = park ? `${park.fullName}` : "U.S. National Parks";
+  // Get park w/ matching code from allNationalParks array obtained when app loads:
+  const park = allNationalParks.filter((park) => park.parkCode === parkCode)[0];
 
-  // Retrieve current park:
-  /* Make separate request here, instead of getting park from initial request when app loads, in order to get the most up-to-date weather info */
-  useEffect(() => {
-    getParkByCode(parkCode)
-      .then((response) => {
-        if (response.status === 429) {
-          setErrorCode("429");
-        }
-        return response.text();
-      })
-      .then((result) => {
-        setPark(JSON.parse(result).data[0]);
-      })
-      .catch((error) => console.log(error))
-      .finally(() => setParkIsLoading(false));
-  }, [parkCode, park]);
+  document.title = `${park.fullName}`;
 
-  // Retrieve park's current weather:
+  // Get park's current weather:
   useEffect(() => {
-    getParkCurrentWeather(park?.latitude, park?.longitude)
+    getParkCurrentWeather(park.latitude, park.longitude)
       .then((response) => response.text())
       .then((result) => setParkWeather(JSON.parse(result)))
       .catch((error) => {
         console.log(error);
         setWasErrorFetchingWeather(true);
       });
-  }, [park]);
+  }, [park.latitude, park.longitude]);
 
-  const stateIndices: string[] | undefined = park?.states
+  const stateIndices: string[] = park.states
     .replace(/,/g, " ")
     .split(" ")
     .filter((index) => Object.keys(stateFilterOptions).includes(index));
-  const parkStates: string[] | undefined = stateIndices?.map(
+  const parkStates: string[] | undefined = stateIndices.map(
     (index) => stateFilterOptions[index as keyof typeof stateFilterOptions]
   );
 
-  const territoryIndices: string[] | undefined = park?.states
+  const territoryIndices: string[] = park.states
     .replace(/,/g, " ")
     .split(" ")
     .filter((index) => Object.keys(territoryFilterOptions).includes(index));
@@ -92,156 +77,141 @@ export const ParkPage = () => {
     (index) => territoryFilterOptions[index as keyof typeof territoryFilterOptions]
   );
 
-  // Process of adding '&' to last item in locations array:
-  const inStates = !parkIsLoading && parkStates && parkStates?.length > 0;
-  const inTerritories = !parkIsLoading && parkTerritories && parkTerritories?.length > 0;
-
+  // Process of adding '&' to last item in locations array if park is located in 2+ territories/states:
+  // EX: Michigan & Wisconsin
+  // EX: Alaska
+  // EX: Colorado, Arizona, & New Mexico
+  const inStates: boolean = parkStates.length > 0;
+  const inTerritories: boolean = parkTerritories.length > 0;
   // Set initial value (most likely case):
-  let locations: string[] | undefined = parkStates;
+  let locations: string[] = parkStates;
   // Concat parkStates & parkTerritories arrays if park's in at least one state and at least one territory:
   if (inStates && inTerritories) {
-    locations = parkStates?.concat(parkTerritories);
+    locations = parkStates.concat(parkTerritories);
     // Else, set to parkTerritories if only in territories and no states:
   } else if (inTerritories && !inStates) {
     locations = parkTerritories;
   }
-
   let lastLocation: string;
-  if (!parkIsLoading && locations && locations.length > 1) {
+  if (locations.length > 1) {
     // Get last element in locations array:
     lastLocation = locations[locations.length - 1];
     // Get array of locations excluding lastLocation:
-    locations = locations?.filter((location) => location !== lastLocation);
+    locations = locations.filter((location) => location !== lastLocation);
     // Add " & " to lastLocation:
     lastLocation = ` & ${lastLocation}`;
     // Add lastLocation to end of locations array:
     locations.push(lastLocation);
   }
 
-  const parkAlerts: TParkAlert[] = allNPAlerts.filter(
-    (alert) => alert.parkCode === parkCode
-  );
-  const areAlerts: boolean = parkAlerts.length > 0;
-
   return (
     <>
       <NavBar notOnHomepage={true} />
       <div className="park-page-main-content-container">
-        {parkIsLoading && !park && <LoadingMessage />}
-        {park && !parkIsLoading && (
-          <>
-            <div className="park-page-headers-container">
-              <h1>{park?.fullName}</h1>
-              <p>
-                Located in{" "}
-                {locations && locations.length > 2 ? locations.join(", ") : locations}
+        <>
+          <div className="park-page-headers-container">
+            <h1>{park.fullName}</h1>
+            <p>
+              Located in{" "}
+              {locations && locations.length > 2 ? locations.join(", ") : locations}
+            </p>
+            <p className="longitude-latitude">
+              Longitude: {`${park.longitude}`} | Latitude: {`${park.latitude}`}
+            </p>
+            <div className="park-google-maps-current-weather-alerts-container">
+              <p
+                title={areAlerts ? "Click to see alerts for this park" : undefined}
+                onClick={areAlerts ? () => setShowAlerts(true) : undefined}
+                className={areAlerts ? "show-modal-open-google-maps" : undefined}
+              >
+                {areAlerts ? "See Current Alerts" : "No Current Alerts"}
               </p>
-              <p className="longitude-latitude">
-                Longitude: {`${park?.longitude}`} | Latitude: {`${park?.latitude}`}
+              <p
+                className="show-modal-open-google-maps"
+                title="Click to see this park's current weather"
+                onClick={() => setShowCurrentWeather(true)}
+              >
+                See Current Weather
               </p>
-              <div className="park-google-maps-current-weather-alerts-container">
-                <p
-                  title={areAlerts ? "Click to see alerts for this park" : undefined}
-                  onClick={areAlerts ? () => setShowAlerts(true) : undefined}
-                  className={areAlerts ? "show-modal-open-google-maps" : undefined}
-                >
-                  {areAlerts ? "See Current Alerts" : "No Current Alerts"}
-                </p>
-                <p
-                  className="show-modal-open-google-maps"
-                  title="Click to see this park's current weather"
-                  onClick={() => setShowCurrentWeather(true)}
-                >
-                  See Current Weather
-                </p>
-                <a
-                  title="Click to open this park's Google Maps page in a new tab"
-                  className="show-modal-open-google-maps"
-                  target="_blank"
-                  href={`https://www.google.com/maps/search/?api=1&query=${park?.fullName
-                    .toLowerCase()
-                    .replace(/\s/g, "+")}`}
-                >
-                  See on Google Maps
-                </a>
+              <a
+                title="Click to open this park's Google Maps page in a new tab"
+                className="show-modal-open-google-maps"
+                target="_blank"
+                href={`https://www.google.com/maps/search/?api=1&query=${park.fullName
+                  .toLowerCase()
+                  .replace(/\s/g, "+")}`}
+              >
+                See on Google Maps
+              </a>
+            </div>
+          </div>
+          <div className="park-page-main-content-container">
+            <div className="park-page-top-section">
+              <div className="park-page-img-slideshow-container">
+                <ImageSlideshow park={park} showCaption={true} />
+              </div>
+              <div className="park-basic-info-container">
+                <header>Description</header>
+                <p>{park.description}</p>
+                <header>General Weather Info</header>
+                <p>{park.weatherInfo}</p>
               </div>
             </div>
-            {park && (
-              <div className="park-page-main-content-container">
-                <div className="park-page-top-section">
-                  <div className="park-page-img-slideshow-container">
-                    <ImageSlideshow park={park} showCaption={true} />
-                  </div>
-                  <div className="park-basic-info-container">
-                    <header>Description</header>
-                    <p>{park.description}</p>
-                    <header>General Weather Info</header>
-                    <p>{park.weatherInfo}</p>
-                  </div>
-                </div>
-                {showAlerts && (
-                  <ParkAlerts
-                    parkName={park.fullName}
-                    setShowAlerts={setShowAlerts}
-                    parkCode={parkCode}
-                  />
-                )}
-                {showCurrentWeather && (
-                  <ParkCurrentWeather
-                    setShowCurrentWeather={setShowCurrentWeather}
-                    parkWeather={parkWeather}
-                    parkName={park.fullName}
-                    wasErrorFetchingWeather={wasErrorFetchingWeather}
-                  />
-                )}
-                <div className="park-page-bottom-section">
-                  <DropdownButton
-                    text="Activities"
-                    action={() => setShowActivities(!showActivities)}
-                    title={showActivities ? "Hide Activities" : "Show Activities"}
-                    showItems={showActivities}
-                    numberOfItems={park.activities.length}
-                  />
-                  <ParkActivities showActivities={showActivities} park={park} />
-                  <DropdownButton
-                    action={() => setShowEntranceFees(!showEntranceFees)}
-                    text="Entrance Fees"
-                    title={showEntranceFees ? "Hide Entrance Fees" : "Show Entrance Fees"}
-                    showItems={showEntranceFees}
-                    numberOfItems={park.entranceFees.length}
-                  />
-                  <ParkEntranceFees park={park} showEntranceFees={showEntranceFees} />
-                  <DropdownButton
-                    text="Available Passes"
-                    action={() => setShowEntrancePasses(!showEntrancePasses)}
-                    title={
-                      showEntrancePasses
-                        ? "Hide Available Passes"
-                        : "Show Available Passes"
-                    }
-                    showItems={showEntrancePasses}
-                    numberOfItems={park.entrancePasses.length}
-                  />
-                  <ParkEntrancePasses
-                    park={park}
-                    showEntrancePasses={showEntrancePasses}
-                  />
-                  <DropdownButton
-                    text="Contact Info"
-                    action={() => setShowContactInfo(!showContactInfo)}
-                    title={showContactInfo ? "Hide Contact Info" : "Hide Contact Info"}
-                    showItems={showContactInfo}
-                    numberOfItems={Object.keys(park.contacts).length}
-                  />
-                  <ParkContacts park={park} showContactInfo={showContactInfo} />
-                </div>
-              </div>
+            {showAlerts && (
+              <ParkAlerts
+                parkName={park.fullName}
+                setShowAlerts={setShowAlerts}
+                parkCode={parkCode}
+              />
             )}
-          </>
-        )}
-        {!parkIsLoading && !park && <FailFetchMessage errorCode={errorCode} />}
+            {showCurrentWeather && (
+              <ParkCurrentWeather
+                setShowCurrentWeather={setShowCurrentWeather}
+                parkWeather={parkWeather}
+                parkName={park.fullName}
+                wasErrorFetchingWeather={wasErrorFetchingWeather}
+              />
+            )}
+            <div className="park-page-bottom-section">
+              <DropdownButton
+                text="Activities"
+                action={() => setShowActivities(!showActivities)}
+                title={showActivities ? "Hide Activities" : "Show Activities"}
+                showItems={showActivities}
+                numberOfItems={park.activities.length}
+              />
+              <ParkActivities showActivities={showActivities} park={park} />
+              <DropdownButton
+                action={() => setShowEntranceFees(!showEntranceFees)}
+                text="Entrance Fees"
+                title={showEntranceFees ? "Hide Entrance Fees" : "Show Entrance Fees"}
+                showItems={showEntranceFees}
+                numberOfItems={park.entranceFees.length}
+              />
+              <ParkEntranceFees park={park} showEntranceFees={showEntranceFees} />
+              <DropdownButton
+                text="Available Passes"
+                action={() => setShowEntrancePasses(!showEntrancePasses)}
+                title={
+                  showEntrancePasses ? "Hide Available Passes" : "Show Available Passes"
+                }
+                showItems={showEntrancePasses}
+                numberOfItems={park.entrancePasses.length}
+              />
+              <ParkEntrancePasses park={park} showEntrancePasses={showEntrancePasses} />
+              <DropdownButton
+                text="Contact Info"
+                action={() => setShowContactInfo(!showContactInfo)}
+                title={showContactInfo ? "Hide Contact Info" : "Hide Contact Info"}
+                showItems={showContactInfo}
+                numberOfItems={Object.keys(park.contacts).length}
+              />
+              <ParkContacts park={park} showContactInfo={showContactInfo} />
+            </div>
+          </div>
+        </>
       </div>
-      {!parkIsLoading && park && <UniversalFooter />}
+      <UniversalFooter />
     </>
   );
 };
