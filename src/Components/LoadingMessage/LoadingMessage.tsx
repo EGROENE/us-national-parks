9b95b1a6faa5013c9665e55dad310af620d64b3e
Ethan Groene <ethangroene@gmail.com>
2024-02-01 12:34:16 -0600

    Add,style loading animation

--- src/Components/LoadingMessage/LoadingMessage.tsx
+++ src/Components/LoadingMessage/LoadingMessage.tsx
@@ -1,8 +1,15 @@
 // Should return message & loading animation
 export const LoadingMessage = () => {
   return (
-    <p className="init-loading-or-error-message">
-      Wait a sec while we retrieve the data...
-    </p>
+    <div className="loader-container">
+      <div className="loader">
+        <i className="fas fa-paw paw-1"></i>
+        <i className="fas fa-paw paw-2"></i>
+        <i className="fas fa-paw paw-3"></i>
+      </div>
+      <p className="init-loading-or-error-message">
+        Wait a sec while we retrieve the data...
+      </p>
+    </div>
   );
 };
