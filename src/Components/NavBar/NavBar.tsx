6b4aad639e1f5e3d84b319efa9f56ed8925f90cd
Ethan Groene <ethangroene@gmail.com>
2024-02-06 10:51:27 -0600

    Make links active only if not on pgs they link to

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,20 +1,30 @@
 import { Link } from "react-router-dom";
 
-export const NavBar = ({ notOnHomepage }: { notOnHomepage: boolean }) => {
+export const NavBar = ({
+  notOnHomepage,
+  notOnQuizPage,
+}: {
+  notOnHomepage: boolean;
+  notOnQuizPage: boolean;
+}) => {
   return (
     <div className="navbar">
       {notOnHomepage ? (
         <Link title="To Homepage" to="/">
-          <h1>U.S. National Parks</h1>
+          <h1 className="active-link">U.S. National Parks</h1>
         </Link>
       ) : (
         <h1>U.S. National Parks</h1>
       )}
       <nav>
         <ul>
-          <Link to="/quiz" title="Take the Quiz">
+          {notOnQuizPage ? (
+            <Link to="/quiz" title="Take the Quiz">
+              <li className="active-link">Quiz</li>
+            </Link>
+          ) : (
             <li>Quiz</li>
-          </Link>
+          )}
           <li>Photo Gallery</li>
         </ul>
       </nav>
