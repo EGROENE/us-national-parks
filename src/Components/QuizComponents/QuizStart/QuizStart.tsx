2473a4ed4509484e8d34a5c3c5f6ca863a78746f
Ethan Groene <ethangroene@gmail.com>
2024-02-07 15:12:26 -0600

    Rename state setter

--- src/Components/QuizComponents/QuizStart/QuizStart.tsx
+++ src/Components/QuizComponents/QuizStart/QuizStart.tsx
@@ -1,15 +1,15 @@
 const QuizStart = ({
-  setTotalQuestions,
+  setTotalQuestionsSelected,
 }: {
-  setTotalQuestions: React.Dispatch<React.SetStateAction<number | undefined>>;
+  setTotalQuestionsSelected: React.Dispatch<React.SetStateAction<number | undefined>>;
 }) => {
   return (
     <>
       <h1>Ready to test your knowledge on U.S. National Parks?</h1>
       <h2>Select a number of questions to play & let's get started!</h2>
       <div>
-        <button onClick={() => setTotalQuestions(15)}>15</button>
-        <button onClick={() => setTotalQuestions(30)}>30</button>
+        <button onClick={() => setTotalQuestionsSelected(15)}>15</button>
+        <button onClick={() => setTotalQuestionsSelected(30)}>30</button>
       </div>
     </>
   );
