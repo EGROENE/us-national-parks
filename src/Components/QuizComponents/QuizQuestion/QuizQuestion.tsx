a562febb9b61cedd45bb3e8be6af94192edc4a1b
Ethan Groene <ethangroene@gmail.com>
2024-02-07 16:26:43 -0600

    Style questions,elems;change navigation
    Styling includes general appearance of quiz questions & also makes clear after user answers question if they were correct or not. Comment about question (if any) displays after answering.Nav changes:btn to advance or restart.

--- src/Components/QuizComponents/QuizQuestion/QuizQuestion.tsx
+++ src/Components/QuizComponents/QuizQuestion/QuizQuestion.tsx
@@ -1,6 +1,7 @@
+import { useEffect, useState } from "react";
 import { TQuizQuestion } from "../../../types";
-
 import { shuffleQuizAnswersArray } from "../../../methods";
+import { quizBackgroundImages } from "../../../constants";
 
 const QuizQuestion = ({
   question,
@@ -8,37 +9,84 @@ const QuizQuestion = ({
   setQuestionIndex,
   score,
   setScore,
+  quizLength,
 }: {
   question: TQuizQuestion | undefined;
   questionIndex: number;
   setQuestionIndex: React.Dispatch<React.SetStateAction<number>>;
   score: number;
   setScore: React.Dispatch<React.SetStateAction<number>>;
+  quizLength: number | undefined;
 }) => {
-  // randomize order of question's answers:
-  let randomizedAnswers: [string, string][] | undefined;
-  if (question) {
-    randomizedAnswers = shuffleQuizAnswersArray(Object.entries(question.answers));
-  }
+  const [questionAnswered, setQuestionAnswered] = useState<boolean>(false);
+  const [randomizedAnswers, setRandomizedAnswers] = useState<
+    [string, string][] | undefined
+  >();
+  const [selectedAnswer, setSelectedAnswer] = useState<[string, string]>(["", ""]);
+
+  const setBackgroundImage = () => {
+    const randNum = Math.floor(Math.random() * quizBackgroundImages.length);
+    document.body.style.backgroundImage = `url(${quizBackgroundImages[randNum]})`;
+  };
+
+  useEffect(() => {
+    setBackgroundImage();
+    setQuestionAnswered(false);
+    setSelectedAnswer(["", ""]);
+    if (question) {
+      setRandomizedAnswers(shuffleQuizAnswersArray(Object.entries(question.answers)));
+    }
+  }, [question]);
 
   const handleAnswer = (answer: [string, string]): void => {
-    setQuestionIndex(questionIndex + 1);
+    setQuestionAnswered(true);
+    setSelectedAnswer(answer);
     if (answer[0] === "rightAnswer") {
       setScore(score + 1);
     }
   };
 
+  const answerClass = (
+    answer: [string, string]
+  ): "answer correct" | "answer incorrect" | "answer" => {
+    if (questionAnswered) {
+      if (answer[0] === "rightAnswer") {
+        return "answer correct";
+      } else if (answer[0] !== "rightAnswer" && answer === selectedAnswer) {
+        return "answer incorrect";
+      }
+    }
+    return "answer";
+  };
+
   return (
-    <>
-      <h1>{question?.question}</h1>
+    <div className="quiz-question">
+      <header>{question?.question}</header>
       <div className="quiz-questions-container">
         {randomizedAnswers?.map((answer: [string, string]) => (
-          <button key={answer[0]} onClick={() => handleAnswer(answer)}>
+          <button
+            disabled={questionAnswered}
+            className={answerClass(answer)}
+            key={answer[0]}
+            onClick={() => handleAnswer(answer)}
+          >
             {answer[1]}
           </button>
         ))}
       </div>
-    </>
+      {questionAnswered && question?.comment}
+      <div className="quiz-nav-btns-container">
+        <button onClick={() => window.location.reload()}>Restart Quiz</button>
+        <button
+          disabled={!questionAnswered}
+          onClick={() => setQuestionIndex(questionIndex + 1)}
+        >
+          {quizLength && questionIndex < quizLength - 1
+            ? "Next Question"
+            : "See Results!"}
+        </button>
+      </div>
+    </div>
   );
 };
 
