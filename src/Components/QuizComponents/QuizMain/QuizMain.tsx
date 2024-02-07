3b9491e296d61218f8007301740698381e058f92
Ethan Groene <ethangroene@gmail.com>
2024-02-06 21:12:40 -0600

    Render quiz question
    Current question displays when user has begun quiz & when quiz is not yet finished.

--- src/Components/QuizComponents/QuizMain/QuizMain.tsx
+++ src/Components/QuizComponents/QuizMain/QuizMain.tsx
@@ -7,24 +7,42 @@ import QuizStart from "../QuizStart/QuizStart";
 import { quizQuestions } from "../../../constants";
 
 // Methods:
-import { shuffleArray } from "../../../methods";
+import { shuffleQuestionsArray } from "../../../methods";
 
 // Types:
 import { TQuizQuestion } from "../../../types";
+import QuizQuestion from "../QuizQuestion/QuizQuestion";
 
 export const QuizMain = () => {
   const [quizLength, setQuizLength] = useState<number | undefined>();
+  const [questionIndex, setQuestionIndex] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
 
-  const randomizedQuestions: unknown[] = shuffleArray(quizQuestions);
+  const randomizedQuestions = shuffleQuestionsArray(quizQuestions);
 
   const currentQuestions: TQuizQuestion[] = randomizedQuestions.filter(
     (question) => quizLength && randomizedQuestions.indexOf(question) < quizLength
   );
 
+  // Variables pertaining to logic that determines parts of quiz that should render (done to simplify return statement below):
+  const quizNotStarted: boolean = !quizLength;
+  const quizIsOver: boolean = questionIndex >= currentQuestions.length - 1;
+  const quizBegunAndNotFinished: boolean = quizLength !== undefined && !quizIsOver;
+
   return (
     <>
       <NavBar notOnHomepage={true} notOnQuizPage={false} />
-      {!quizLength && <QuizStart setQuizLength={setQuizLength} />}
+      {quizNotStarted && <QuizStart setQuizLength={setQuizLength} />}
+      {quizBegunAndNotFinished && (
+        <QuizQuestion
+          question={currentQuestions[questionIndex]}
+          questionIndex={questionIndex}
+          setQuestionIndex={setQuestionIndex}
+          score={score}
+          setScore={setScore}
+        />
+      )}
+      {/* render <QuizEnding /> when quizIsOver */}
     </>
   );
 };
