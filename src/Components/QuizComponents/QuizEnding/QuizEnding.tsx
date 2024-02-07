ad017d72cc827267c4de5a71516789342ae1ea3e
Ethan Groene <ethangroene@gmail.com>
2024-02-06 22:43:20 -0600

    Add comp to show user final score

---
+++ src/Components/QuizComponents/QuizEnding/QuizEnding.tsx
@@ -0,0 +1,27 @@
+const QuizEnding = ({ finalScore }: { finalScore: number }) => {
+  let feedback: string = "";
+  switch (true) {
+    case finalScore === 1:
+      feedback = "Perfect score! You must be really smart.";
+      break;
+    case finalScore < 0.9 && finalScore >= 0.75:
+      feedback = "Not bad!";
+      break;
+    case finalScore < 0.75 && finalScore >= 0.5:
+      feedback = "Could be better.";
+      break;
+    case finalScore < 0.5 && finalScore >= 0.2:
+      feedback = "You have some studying to do.";
+      break;
+    default:
+      feedback = "Abysmal.";
+  }
+  return (
+    <>
+      <h1>Thanks for playing the quiz! Your final score is:</h1>
+      <h2>{(finalScore * 100).toFixed(2)}</h2>
+      <p>{feedback}</p>
+    </>
+  );
+};
+export default QuizEnding;
