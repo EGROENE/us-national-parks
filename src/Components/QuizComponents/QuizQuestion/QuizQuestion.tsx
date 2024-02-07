433e3b3ecb0075686dc26af78fbd221d3ebcb9b1
Ethan Groene <ethangroene@gmail.com>
2024-02-06 21:21:39 -0600

    Add comp to display a question,its answers
    Question displays as header, answer options are randomized. If answer is right, score is incremented by 1. In any case, question index is upped by 1 once user clicks an option, so next Q displays.

---
+++ src/Components/QuizComponents/QuizQuestion/QuizQuestion.tsx
@@ -0,0 +1,44 @@
+import { TQuizQuestion } from "../../../types";
+
+import { shuffleAnswersArray } from "../../../methods";
+
+const QuizQuestion = ({
+  question,
+  questionIndex,
+  setQuestionIndex,
+  score,
+  setScore,
+}: {
+  question: TQuizQuestion;
+  questionIndex: number;
+  setQuestionIndex: React.Dispatch<React.SetStateAction<number>>;
+  score: number;
+  setScore: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+  // randomize order of question's answers:
+  const randomizedAnswers: [string, string][] = shuffleAnswersArray(
+    Object.entries(question.answers)
+  );
+
+  const handleAnswer = (answer: [string, string]): void => {
+    setQuestionIndex(questionIndex + 1);
+    if (answer[0] === "rightAnswer") {
+      setScore(score + 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>{question.question}</h1>
+      <div className="quiz-questions-container">
+        {randomizedAnswers.map((answer: [string, string]) => (
+          <button key={answer[0]} onClick={() => handleAnswer(answer)}>
+            {answer[1]}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default QuizQuestion;
