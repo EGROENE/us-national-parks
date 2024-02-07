03996ef055763eb31af4d68f26fb4b8e9677b85d
Ethan Groene <ethangroene@gmail.com>
2024-02-06 22:49:57 -0600

    Render QuizEnding properly,simplify logic
    Also, change name of a state value & its setter.

--- src/Components/QuizComponents/QuizMain/QuizMain.tsx
+++ src/Components/QuizComponents/QuizMain/QuizMain.tsx
@@ -12,27 +12,29 @@ import { shuffleQuestionsArray } from "../../../methods";
 // Types:
 import { TQuizQuestion } from "../../../types";
 import QuizQuestion from "../QuizQuestion/QuizQuestion";
+import QuizEnding from "../QuizEnding/QuizEnding";
 
 export const QuizMain = () => {
-  const [quizLength, setQuizLength] = useState<number | undefined>();
+  const [totalQuestions, setTotalQuestions] = useState<number | undefined>();
   const [questionIndex, setQuestionIndex] = useState<number>(0);
   const [score, setScore] = useState<number>(0);
 
   const randomizedQuestions = shuffleQuestionsArray(quizQuestions);
 
   const currentQuestions: TQuizQuestion[] = randomizedQuestions.filter(
-    (question) => quizLength && randomizedQuestions.indexOf(question) < quizLength
+    (question) => totalQuestions && randomizedQuestions.indexOf(question) < totalQuestions
   );
+  const quizLength = currentQuestions.length;
 
-  // Variables pertaining to logic that determines parts of quiz that should render (done to simplify return statement below):
+  // Variables pertaining to logic that determines parts of quiz that should render (done to make return statement more readable):
   const quizNotStarted: boolean = !quizLength;
-  const quizIsOver: boolean = questionIndex > currentQuestions.length - 1;
-  const quizBegunAndNotFinished: boolean = quizLength !== undefined && !quizIsOver;
+  const quizIsOver: boolean = quizLength > 0 && questionIndex + 1 > quizLength;
+  const quizBegunAndNotFinished: boolean = quizLength > 0 && !quizIsOver;
 
   return (
     <>
       <NavBar notOnHomepage={true} notOnQuizPage={false} />
-      {quizNotStarted && <QuizStart setQuizLength={setQuizLength} />}
+      {quizNotStarted && <QuizStart setTotalQuestions={setTotalQuestions} />}
       {quizBegunAndNotFinished && (
         <QuizQuestion
           question={currentQuestions[questionIndex]}
@@ -42,7 +44,7 @@ export const QuizMain = () => {
           setScore={setScore}
         />
       )}
-      {/* render <QuizEnding /> when quizIsOver */}
+      {quizIsOver && <QuizEnding finalScore={score / quizLength} />}
     </>
   );
 };
