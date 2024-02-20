36b8b6b8bbd2022cabb224e589b520111dd1af0c
Ethan Groene <ethangroene@gmail.com>
2024-02-19 23:20:40 -0600

    Set random park img as bg in weather modal
    Accounts for imgs that don't exist, but there may be a leaner way to do it.

--- src/Components/ParkPageComponents/ParkPageItems/ParkCurrentWeather/ParkCurrentWeather.tsx
+++ src/Components/ParkPageComponents/ParkPageItems/ParkCurrentWeather/ParkCurrentWeather.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TCurrentWeather } from "../../../../types";
 import WeatherDatapoint from "../WeatherDatapoint/WeatherDatapoint";
 import FailFetchMessage from "../../../VersatileComponents/FailFetchMessage/FailFetchMessage";
@@ -8,11 +8,19 @@ const ParkCurrentWeather = ({
   setShowCurrentWeather,
   parkName,
   wasErrorFetchingWeather,
+  parkImages,
 }: {
   parkWeather: TCurrentWeather | undefined;
   setShowCurrentWeather: React.Dispatch<React.SetStateAction<boolean>>;
   parkName: string;
   wasErrorFetchingWeather: boolean;
+  parkImages: {
+    credit: string;
+    title: string;
+    altText: string;
+    caption: string;
+    url: string;
+  }[];
 }) => {
   // Get park's long/lat as prop from ParkPage:
   const [displayCelsius, setDisplayCelsius] = useState<boolean>(false);
@@ -20,6 +28,7 @@ const ParkCurrentWeather = ({
   const [windInKM, setWindInKM] = useState<boolean>(false);
   const [precipitationInMetric, setPrecipitationInMetric] = useState<boolean>(false);
   const [pressureInMetric, setPressureInMetric] = useState<boolean>(false);
+  const [backgroundImage, setBackgroundImage] = useState<string>("");
 
   let lastUpdatedTime: Date | string | undefined;
   if (parkWeather) {
@@ -27,129 +36,163 @@ const ParkCurrentWeather = ({
     lastUpdatedTime = `${lastUpdatedTime?.toLocaleDateString()} at ${lastUpdatedTime?.toLocaleTimeString()}`;
   }
 
+  const backgroundImageExists = (img: string): boolean => {
+    let exists: boolean = false;
+    fetch(img)
+      .then(() => (exists = true))
+      .catch(() => (exists = false));
+    return exists;
+  };
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * parkImages.length);
+    console.log(randomIndex);
+    console.log(randomIndex + 1);
+    console.log(parkImages.length);
+    console.log(backgroundImageExists(parkImages[randomIndex].url));
+    console.log(parkImages[3].url);
+    console.log(parkImages[4].url);
+    if (backgroundImageExists(parkImages[randomIndex].url)) {
+      console.log("exists");
+      setBackgroundImage(parkImages[randomIndex].url);
+    } else {
+      console.log("doesn't exist");
+      if (randomIndex + 1 === parkImages.length) {
+        setBackgroundImage(parkImages[0].url);
+      } else {
+        setBackgroundImage(parkImages[randomIndex + 1].url);
+      }
+    }
+  }, [setBackgroundImage, parkImages]);
+
   return (
     <div className="current-weather-modal-hero">
-      <div className="current-weather-container">
-        <i
-          onClick={() => setShowCurrentWeather(false)}
-          title="Close Current Weather"
-          className="fas fa-times"
-        ></i>
-        <header className="park-modal-intro-header" style={{ color: "white" }}>
-          Weather in
-        </header>
-        <header className="park-modal-header" style={{ color: "white" }}>
-          {parkName}
-        </header>
-        {wasErrorFetchingWeather ? (
-          <FailFetchMessage margin="1rem 0" buttonColor="var(--theme-blue-dark)" />
-        ) : (
-          <>
-            {" "}
-            <p>Last Updated: {`${lastUpdatedTime}`}</p>
-            <div className="weather-info-container">
-              <div className="extended-weather-info">
-                <p>UV Index: {parkWeather?.current.uv.toFixed(1)}</p>
-                <p>Humidity: {parkWeather?.current.humidity}%</p>
-                <WeatherDatapoint
-                  dataLabel="Visibility: "
-                  dataIsDisplayed={visibilityInKM}
-                  setDataIsDisplayed={setVisibilityKM}
-                  data={{
-                    datumOne: parkWeather?.current.vis_miles,
-                    datumTwo: parkWeather?.current.vis_km,
-                  }}
-                  units={{ unitOne: "mi", unitTwo: "km" }}
-                  separator=" | "
-                  stylingClass="selected-unit"
-                />
-                <WeatherDatapoint
-                  dataLabel="Gust: "
-                  dataIsDisplayed={windInKM}
-                  setDataIsDisplayed={setWindInKM}
-                  data={{
-                    datumOne: parkWeather?.current.gust_mph,
-                    datumTwo: parkWeather?.current.gust_kph,
-                  }}
-                  units={{ unitOne: "mph", unitTwo: "km/h" }}
-                  separator=" | "
-                  stylingClass="selected-unit"
-                />
-                <WeatherDatapoint
-                  dataLabel="Precipitation: "
-                  dataIsDisplayed={precipitationInMetric}
-                  setDataIsDisplayed={setPrecipitationInMetric}
-                  data={{
-                    datumOne: parkWeather?.current.precip_in,
-                    datumTwo: parkWeather?.current.precip_mm,
-                  }}
-                  units={{ unitOne: "in", unitTwo: "mm" }}
-                  separator=" | "
-                  stylingClass="selected-unit"
-                />
-                <WeatherDatapoint
-                  dataLabel="Pressure: "
-                  dataIsDisplayed={pressureInMetric}
-                  setDataIsDisplayed={setPressureInMetric}
-                  data={{
-                    datumOne: parkWeather?.current.pressure_in,
-                    datumTwo: parkWeather?.current.pressure_mb,
-                  }}
-                  units={{ unitOne: "in", unitTwo: "mbar" }}
-                  separator=" | "
-                  stylingClass="selected-unit"
-                />
-              </div>
-              <div className="basic-weather-info-container">
-                <div className="weather-img-container">
-                  <img src={parkWeather?.current.condition.icon} />
-                </div>
-                <div className="basic-weather-info-text-container">
+      <div
+        className="current-weather-background"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div className="current-weather-container">
+          <i
+            onClick={() => setShowCurrentWeather(false)}
+            title="Close Current Weather"
+            className="fas fa-times"
+          ></i>
+          <header className="park-modal-intro-header" style={{ color: "white" }}>
+            Weather in
+          </header>
+          <header className="park-modal-header" style={{ color: "white" }}>
+            {parkName}
+          </header>
+          {wasErrorFetchingWeather ? (
+            <FailFetchMessage margin="1rem 0" buttonColor="var(--theme-blue-dark)" />
+          ) : (
+            <>
+              {" "}
+              <p>Last Updated: {`${lastUpdatedTime}`}</p>
+              <div className="weather-info-container">
+                <div className="extended-weather-info">
+                  <p>UV Index: {parkWeather?.current.uv.toFixed(1)}</p>
+                  <p>Humidity: {parkWeather?.current.humidity}%</p>
                   <WeatherDatapoint
-                    datapoint="current-temp"
-                    dataIsDisplayed={displayCelsius}
-                    setDataIsDisplayed={setDisplayCelsius}
+                    dataLabel="Visibility: "
+                    dataIsDisplayed={visibilityInKM}
+                    setDataIsDisplayed={setVisibilityKM}
                     data={{
-                      datumOne: parkWeather?.current.temp_f,
-                      datumTwo: parkWeather?.current.feelslike_c,
+                      datumOne: parkWeather?.current.vis_miles,
+                      datumTwo: parkWeather?.current.vis_km,
                     }}
-                    units={{ unitOne: "F", unitTwo: "C" }}
-                    dataMetric="°"
-                    separator=" | "
-                    stylingClass="selected-unit"
-                  />
-                  <p>{parkWeather?.current.condition.text}</p>
-                  <WeatherDatapoint
-                    dataLabel="Feels like: "
-                    dataIsDisplayed={displayCelsius}
-                    setDataIsDisplayed={setDisplayCelsius}
-                    data={{
-                      datumOne: parkWeather?.current.temp_f,
-                      datumTwo: parkWeather?.current.feelslike_c,
-                    }}
-                    units={{ unitOne: "F", unitTwo: "C" }}
-                    dataMetric="°"
+                    units={{ unitOne: "mi", unitTwo: "km" }}
                     separator=" | "
                     stylingClass="selected-unit"
                   />
                   <WeatherDatapoint
-                    dataLabel="Wind: "
+                    dataLabel="Gust: "
                     dataIsDisplayed={windInKM}
                     setDataIsDisplayed={setWindInKM}
                     data={{
-                      datumOne: parkWeather?.current.wind_mph,
-                      datumTwo: parkWeather?.current.wind_kph,
+                      datumOne: parkWeather?.current.gust_mph,
+                      datumTwo: parkWeather?.current.gust_kph,
                     }}
                     units={{ unitOne: "mph", unitTwo: "km/h" }}
                     separator=" | "
                     stylingClass="selected-unit"
                   />
+                  <WeatherDatapoint
+                    dataLabel="Precipitation: "
+                    dataIsDisplayed={precipitationInMetric}
+                    setDataIsDisplayed={setPrecipitationInMetric}
+                    data={{
+                      datumOne: parkWeather?.current.precip_in,
+                      datumTwo: parkWeather?.current.precip_mm,
+                    }}
+                    units={{ unitOne: "in", unitTwo: "mm" }}
+                    separator=" | "
+                    stylingClass="selected-unit"
+                  />
+                  <WeatherDatapoint
+                    dataLabel="Pressure: "
+                    dataIsDisplayed={pressureInMetric}
+                    setDataIsDisplayed={setPressureInMetric}
+                    data={{
+                      datumOne: parkWeather?.current.pressure_in,
+                      datumTwo: parkWeather?.current.pressure_mb,
+                    }}
+                    units={{ unitOne: "in", unitTwo: "mbar" }}
+                    separator=" | "
+                    stylingClass="selected-unit"
+                  />
+                </div>
+                <div className="basic-weather-info-container">
+                  <div className="weather-img-container">
+                    <img src={parkWeather?.current.condition.icon} />
+                  </div>
+                  <div className="basic-weather-info-text-container">
+                    <WeatherDatapoint
+                      datapoint="current-temp"
+                      dataIsDisplayed={displayCelsius}
+                      setDataIsDisplayed={setDisplayCelsius}
+                      data={{
+                        datumOne: parkWeather?.current.temp_f,
+                        datumTwo: parkWeather?.current.feelslike_c,
+                      }}
+                      units={{ unitOne: "F", unitTwo: "C" }}
+                      dataMetric="°"
+                      separator=" | "
+                      stylingClass="selected-unit"
+                    />
+                    <p>{parkWeather?.current.condition.text}</p>
+                    <WeatherDatapoint
+                      dataLabel="Feels like: "
+                      dataIsDisplayed={displayCelsius}
+                      setDataIsDisplayed={setDisplayCelsius}
+                      data={{
+                        datumOne: parkWeather?.current.temp_f,
+                        datumTwo: parkWeather?.current.feelslike_c,
+                      }}
+                      units={{ unitOne: "F", unitTwo: "C" }}
+                      dataMetric="°"
+                      separator=" | "
+                      stylingClass="selected-unit"
+                    />
+                    <WeatherDatapoint
+                      dataLabel="Wind: "
+                      dataIsDisplayed={windInKM}
+                      setDataIsDisplayed={setWindInKM}
+                      data={{
+                        datumOne: parkWeather?.current.wind_mph,
+                        datumTwo: parkWeather?.current.wind_kph,
+                      }}
+                      units={{ unitOne: "mph", unitTwo: "km/h" }}
+                      separator=" | "
+                      stylingClass="selected-unit"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <p className="weather-source">Data provided by weatherapi.com</p>
-          </>
-        )}
+              <p className="weather-source">Data provided by weatherapi.com</p>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
