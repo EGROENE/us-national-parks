1b17efff123ed594f5327be71e67057ac3a8ad78
Ethan Groene <ethangroene@gmail.com>
2024-02-20 11:16:47 -0600

    Shorten class name

--- src/Components/VersatileComponents/FailFetchMessage/FailFetchMessage.tsx
+++ src/Components/VersatileComponents/FailFetchMessage/FailFetchMessage.tsx
@@ -9,7 +9,7 @@ const FailFetchMessage = ({
 }) => {
   return (
     <div className="fail-fetch-message-container">
-      <p style={{ margin: margin }} className="init-loading-or-error-message">
+      <p style={{ margin: margin }} className="fetch-error-message">
         {isError429
           ? "Too many requests have been made to the server. Please try again later."
           : "Sorry, there was an error in fetching the data. Please reload the page."}
