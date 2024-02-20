d1ccc6639435deffe1eb491e340e3f0ae504a44c
Ethan Groene <ethangroene@gmail.com>
2024-02-20 11:33:51 -0600

    Style fetch err msg w/ only CSS,not props

--- src/Components/VersatileComponents/FailFetchMessage/FailFetchMessage.tsx
+++ src/Components/VersatileComponents/FailFetchMessage/FailFetchMessage.tsx
@@ -1,24 +1,12 @@
-const FailFetchMessage = ({
-  isError429,
-  margin,
-  buttonColor,
-}: {
-  isError429?: boolean;
-  margin?: string;
-  buttonColor?: string;
-}) => {
+const FailFetchMessage = ({ isError429 }: { isError429?: boolean }) => {
   return (
     <div className="fail-fetch-message-container">
-      <p style={{ margin: margin }} className="fetch-error-message">
+      <p className="fetch-error-message">
         {isError429
           ? "Too many requests have been made to the server. Please try again later."
           : "Sorry, there was an error in fetching the data. Please reload the page."}
       </p>
-      <button
-        className="reload-btn"
-        style={{ backgroundColor: buttonColor }}
-        onClick={() => window.location.reload()}
-      >
+      <button className="reload-btn" onClick={() => window.location.reload()}>
         Reload Page
       </button>
     </div>
